2f4922db52ab9d4b5875693b0e737ff042de3618
libjohn <john.little@duke.edu>
2016-08-18 13:17:08 -0400

    building excel read functions

--- introR_workshop-2ndPass.R
+++ introR_workshop-2ndPass.R
@@ -7,6 +7,11 @@ library(readxl)
 
 #where is my data directory?
 getwd()
+DIR <- getwd()
+filename <- "UnempData.xlsx"
+fullpath <- paste(DIR, "/", filename, sep = '')
+# See value of the constructed character vector
+fullpath
 
 # change workding directory if you want
 # setwd()
@@ -14,13 +19,13 @@ getwd()
 # read excel file (read_excel from readxl package)
 ## see how to use it:  
 # in the Console, type ?read_excel
-unempdata <- read_excel("C:/Users/jrl/Downloads/UnempData.xlsx")
+unempdata <- read_excel(fullpath)
 
+View(unempdata)
 class(unempdata)
 dim(unempdata)
 nrow(unempdata)
 ncol(unempdata)
 str(unempdata)
-View(unempdata)
 summary(unempdata)
 
